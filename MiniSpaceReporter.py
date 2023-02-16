e28d276dd4ae0c33194af5aa917c6c9aca9ab7a4
StuartMason21 <stuartstuster@hotmail.com>
2023-02-16 13:08:19 +0100

    update gitignore update routine

--- MiniSpaceReporter.py
+++ MiniSpaceReporter.py
@@ -1,7 +1,18 @@
 #Storage Info for a given path. Useful for DFS name spaces.
 import shutil
+import time
 
 def main():
+    while True:
+        print('*********************')
+        get_target_space()
+        user_continue = input('Would you like to get the details of another path? Y/N: ')
+        if user_continue.lower() != 'y':
+            print('Thanks for using this program, goodbye!')
+            time.sleep(3)
+            quit()
+
+def get_target_space():
     target = input("please paste your target path: ")
        
     try:
@@ -12,8 +23,10 @@ def main():
         print("Total: %d GB" % (total // (2**30)))
         print("Used: %d GB" % (used // (2**30)))
         print("Free: %d GB" % (free // (2**30)))
+        print('*********************')
     except Exception as e:
         print (e)
+        print('*********************')
 
 if __name__ == '__main__':
     main()
